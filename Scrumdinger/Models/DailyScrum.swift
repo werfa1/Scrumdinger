ab2165f1cb4ce271c31030bbd7a4372956468df4
Pasha Otverchenko <pasha.otverchenko@gmail.com>
2023-06-05 23:49:22 -0700

    Added list

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -7,11 +7,20 @@
 
 import Foundation
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    let id: UUID
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
+    
+    init(title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = UUID()
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
 }
 
 extension DailyScrum {

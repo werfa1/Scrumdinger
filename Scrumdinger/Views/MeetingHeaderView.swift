bd5c80ec8789d0b780f76a0ff5cf26f52262240d
Pasha Otverchenko <pasha.otverchenko@gmail.com>
2023-11-19 21:52:00 -0800

    UI changes. Added custom styles by Apple

---
+++ Scrumdinger/Views/MeetingHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by Pasha Otverchenko on 19.11.2023.
+//
+
+import Foundation
---
+++ Scrumdinger/Views/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  Scrumdinger
+//
+//  Created by Pasha Otverchenko on 21.06.2023.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}

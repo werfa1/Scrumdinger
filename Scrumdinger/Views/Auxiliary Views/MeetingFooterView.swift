39ef586b82e4947ccab045a44b519557da03604e
Pasha Otverchenko <pasha.otverchenko@gmail.com>
2023-12-19 21:51:05 -0800

    Updated section with the actual meeting

---
+++ Scrumdinger/Views/Auxiliary Views/MeetingFooterView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingFooterView.swift
+//  Scrumdinger
+//
+//  Created by Pasha Otverchenko on 27.11.2023.
+//
+
+import SwiftUI
+
+struct MeetingFooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingFooterView()
+}
